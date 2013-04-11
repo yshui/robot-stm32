bd61856df2c8dc914f14719fa8887aaea6276404
Wandsea <wandsea@gmail.com>
2013-04-11 11:43:28 +0800

    init repo

---
+++ APP/app.h
@@ -0,0 +1,8 @@
+#ifndef	_APP_H_
+#define	_APP_H_
+
+/**************** 用户任务声明 *******************/
+void Task_Start(void *p_arg);
+void Task_LED2(void *p_arg);
+void Task_LED3(void *p_arg);
+#endif	//_APP_H_
---
+++ USER/includes.h
@@ -0,0 +1,13 @@
+#ifndef  __INCLUDES_H__
+#define  __INCLUDES_H__
+
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+
+#include  "ucos_ii.h"  		//uC/OS-II系统函数头文件
+
+#include  	"BSP.h"			//与开发板相关的函数
+#include 	"app.h"			//用户任务函数
+#include 	"led.h"			//LED驱动函数
+
+#endif //__INCLUDES_H__
